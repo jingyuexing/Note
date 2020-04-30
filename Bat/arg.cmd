1617477d63a177c0f65b051b46c958aabb70b6c4
jingyuexing <1941755817@qq.com>
2020-04-30 18:09:18 +0800

    增加Haxe笔记

---
+++ Bat/arg.cmd
@@ -0,0 +1,11 @@
+@REM @Author: Admin
+@REM @Date:   2019-11-12 23:06:46
+@REM @Last Modified by:   Admin
+@REM Modified time: 2019-11-12 23:15:20
+
+:: 脚本传入参数
+@echo off
+echo %0
+echo %1
+echo %2
+echo %3
